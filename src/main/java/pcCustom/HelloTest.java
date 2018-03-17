2058f49c9c0b7d7a0a7d3b3a40e1698ae29b7366
jiaji Song <jiaji Song>
2018-03-17 23:06:21 +0800

    add gitignore and hellotest

--- src/main/java/pcCustom/HelloTest.java
+++ src/main/java/pcCustom/HelloTest.java
@@ -1,11 +1,26 @@
 package pcCustom;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by sjj on 2018/3/17.
  */
 
-
+@Controller
+@EnableAutoConfiguration
 public class HelloTest {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello pc-custome";
+    }
+    public static void main(String args[]) {
+        SpringApplication.run(HelloTest.class);
+    }
 
 }
