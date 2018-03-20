656f8b2bbd5264a5a2a1ee8a8110e93eea7708db
mccree <song_ji@worksap.co.jp>
2018-03-20 17:20:25 +0800

    add config of driud

--- src/main/java/com/sky/pcCustom/HelloTest.java
+++ src/main/java/com/sky/pcCustom/HelloTest.java
@@ -1,11 +1,14 @@
 package com.sky.pcCustom;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by sjj on 2018/3/17.
